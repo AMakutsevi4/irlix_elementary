a176c0fbbe1bc503e1ba377f37d74d6fb20f30c4
Alexandr <amakutsevich@bk.ru>
2024-07-04 18:32:48 +0700

    first commit

---
+++ src/test/java/ru/irlix_elementary/spring/IrlixElementaryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.irlix_elementary.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrlixElementaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
