ba6f73d6e893245a85249f264685d6186eecbdb5
Alexandr <amakutsevich@bk.ru>
2024-07-08 15:14:35 +0700

    fix hydra annotation Value

--- src/main/java/ru/irlix_elementary/spring/example5/Hydra.java
+++ src/main/java/ru/irlix_elementary/spring/example5/Hydra.java
@@ -1,16 +1,23 @@
 package ru.irlix_elementary.spring.example5;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 import java.util.List;
 
+@Component
 public class Hydra {
 
     private final String name;
     private final List<Head> headList;
 
-
-    public Hydra(String name, List<Head> headList) {
+    @Autowired
+    public Hydra(@Value("${hydra.name}") String name) {
         this.name = name;
-        this.headList = headList;
+        this.headList = List.of(new Head(2, 3),
+                new Head(4, 5),
+                new Head(6, 7));
     }
 
 
--- src/main/java/ru/irlix_elementary/spring/example5/HydraMain.java
+++ src/main/java/ru/irlix_elementary/spring/example5/HydraMain.java
@@ -7,7 +7,7 @@ public class HydraMain {
     public static void main(String[] args) {
         /*Создаем объект context для дальнейшей работы с бинами*/
         ClassPathXmlApplicationContext context =
-                new ClassPathXmlApplicationContext("hydraApplicationContext.xml");
+                new ClassPathXmlApplicationContext("applicationContext.xml");
         /*Достаём бин hydra из контекста*/
         Hydra hydra = context.getBean("hydra", Hydra.class);
         /*Выводим бин на экран*/
